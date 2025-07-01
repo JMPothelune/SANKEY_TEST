280eea7098fff741d28c6ef9e2f58e0322605147
Arthur <tart2000design@gmail.com>
2025-07-01 18:26:30 +0200

    with padding

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -4,7 +4,7 @@ export default function DataPage() {
   return (
     <div className="h-screen flex flex-col">
       <AppNavbar />
-      <div className="flex-1 min-h-0">
+      <div className="flex-1 min-h-0" style={{ paddingTop: 64 }}>
         <iframe
           src="/data/index.html"
           className="w-full h-full border-0"
--- src/app/lots/page.tsx
+++ src/app/lots/page.tsx
@@ -4,7 +4,7 @@ export default function LotsPage() {
   return (
     <div className="h-screen flex flex-col">
       <AppNavbar />
-      <div className="flex-1 min-h-0">
+      <div className="flex-1 min-h-0" style={{ paddingTop: 64 }}>
         <iframe
           src="/lot/index.html"
           className="w-full h-full border-0"
